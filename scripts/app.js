fe8b300f61a2ae03a0f13097a25bfa1b5520ca40
Jim Carucci <jim.carucci@dibbstechnology.com>
2023-03-18 17:41:30 -0500

    pull in data

--- scripts/app.js
+++ scripts/app.js
@@ -34,7 +34,7 @@ if (navigator.mediaDevices.getUserMedia) {
     // Create tone listener object.
     const tones = new ToneListener(stream, [2000, 2500])
 
-    const d = 100  // Frame duration.
+    const d = 1000  // Frame duration.
     const s = 10   // Sample delay.
 
     var asciiBuffer = "";
@@ -48,7 +48,9 @@ if (navigator.mediaDevices.getUserMedia) {
       }));
       console.log(frame);
       if(asciiBuffer.length >= windowSize){
-        console.log(`ASCII Buffer Filled: ${asciiBuffer}`)
+        console.log(`ASCII Buffer Filled: ${asciiBuffer}`);
+        //const test = "01000001"
+        console.log(binaryToText(asciiBuffer));
         asciiBuffer = "";
       }
       if(frame[2000] === true && frame[2500] === true){
@@ -154,6 +156,28 @@ if (navigator.mediaDevices.getUserMedia) {
 
 } else {
    console.log('getUserMedia not supported on your browser!');
+}
+
+function bytesToHex( bytes ) {
+    return bytes.reduce( ( str, byte ) => str + byte.toString( 16 ).padStart( 2, "0" ), "" );
+}
+
+function hexToText( hex ) {
+    var bytes = new Uint8Array(Math.ceil(hex.length / 2));
+    for (var i = 0; i < hex.length; i++) bytes[i] = parseInt(hex.substr(i * 2, 2), 16);
+    var text = new TextDecoder().decode( bytes );
+    return text;
+
+}
+
+function binaryToText(binary) {
+    binary = binary.match(/.{1,8}/g);
+    var arr = [];
+    binary.forEach( function( digit ) {
+        arr.push( parseInt( digit.split('').join(''), 2 ) );
+    });
+    var uint8 = new Uint8Array( arr );
+    return hexToText( bytesToHex( uint8 ) );
 }
 
 function visualize(stream) {
