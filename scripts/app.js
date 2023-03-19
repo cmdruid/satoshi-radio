54e10d5ded5cefedb886ba0f6fcf9b06175f8bb8
Jim Carucci <jim.carucci@dibbstechnology.com>
2023-03-19 16:42:14 -0500

    Attempt at error correction

---
+++ scripts/app.js
@@ -0,0 +1,72 @@
+import { visualize }      from './visualizer.js'
+import { ToneEmitter }    from './tone.js'
+import { ToneController } from './controller.js'
+const { Buff } = window.buffUtils
+
+globalThis.globalStream = null
+
+const msgbox = document.querySelector('#msgBox')
+const feedbox = document.querySelector('#feed')
+
+if (navigator.mediaDevices.getUserMedia) {
+  console.log('getUserMedia supported.');
+
+  const constraints = { audio: true };
+
+  const onSuccess = (stream) => {
+    globalThis.globalStream = stream
+    visualize(stream)
+
+    const emitter = new ToneEmitter(stream)
+
+    let add_to_buffer = false
+    let messageBuffer = ''
+    let message       = ''
+    let feedbuffer = []
+
+    emitter.on('*', (eventName, value) => {
+      feedbuffer.push()
+      console.log(eventName, value)
+    })
+
+    // emitter.on('data', (value) => {
+    //   if ( add_to_buffer ) {
+    //     messageBuffer += value;
+    //     feedbox.innerText = messageBuffer
+    //   }
+
+    //   console.log('emit:', value)
+    // })
+
+    // emitter.on('ctrl', (value) => {
+    //   if (value === 'start') {
+    //     add_to_buffer = true;
+    //     messageBuffer = "";
+    //     console.log('emit: start')
+    //   }
+    //   if (value === 'stop') {
+        
+    //     add_to_buffer = false;
+    //     console.log('emit: stop')
+    //     console.log( 'message buffer:', messageBuffer );
+    //     message = Buff.hex( messageBuffer ).str
+    //     console.log( 'message:', message );
+    //     msgbox.innerText = 'testing!' // message
+    //   }
+    // })
+
+    const controller = new ToneController(emitter)
+
+    emitter.listen()
+  }
+
+  const onError = (err) => {
+    console.log('The following error occured: ' + err);
+  }
+
+  navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+
+} else {
+   console.log('getUserMedia not supported on your browser!');
+}
+
